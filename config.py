3a7505278691eb9561b8816da4b3dad29cd306bc
Huzaifah Nadeem <huzaifahnadeem@gmail.com>
2023-09-28 15:00:51 -0400

    init

--- config.py
+++ config.py
@@ -1,45 +1,6 @@
-import constants
+class plot:
+    # config for the graph plot when visualizing
+    # 'width' x 'height' in inches s.t. each 1 inch is equal to 'dpi' pixels
+    width, height, dpi = 15, 9.75, 80
 
-class data_directory:
-    path = './data'
-
-class graph:
-    class layout:
-        # kamada kawai feeding into fruchterman reingold seems the best option. However, both of these have a higher running time so using just kamada kawai layout for now as that seems to be good by itself as well. fruchterman reingold also seems good but not as good as just kamada kawai 
-        name = constants.graph_layout.kamada_kawaii
-    
-    class nodes:
-        class user_pair:
-            colors = ['cyan', 'green', 'yellow', 'pink', 'red', 'purple', 'blue', 'indigo']
-            size = 700
-            shape = 'o' # circle
-            edge_color = 'black'
-        
-        class storage:
-            color = 'gray'
-            size = 700
-            shape = 's' # square
-            edge_color = 'red'
-        
-        class other:
-            color = 'white'
-            size = 700
-            shape = 'o' # circle
-            edge_color = 'black'
-    
-    class edges:
-        arrow_size = 20
-        # possible styles: ‘-’, ‘–’, ‘-.’, ‘:’ or words like ‘solid’ or ‘dashed’ 
-        style_non_virtual = 'solid'
-        style_virtual = '-.'
-
-        color_non_virtual = 'black'
-        color_virtual = 'red'
-    
-    class plot:
-        # 'width' x 'height' in inches s.t. each 1 inch is equal to 'dpi' pixels
-        width, height, dpi = 15, 9.75, 80
-
-class random_params:
-    # None can be used to use system time for as seed
-    seed = 0
+    layout_choice = 
--- constants.py
+++ constants.py
@@ -1,32 +1,32 @@
-# these classes have variables assigned to the exact string values for ease.
+import networkx as nx
+class layout:
+    def _kk_into_fr(G):
+        # kamada kawai layout for initial pos feed into fruchterman reingold. 
+        # ref: https://en.wikipedia.org/wiki/Force-directed_graph_drawing#:~:text=A%20combined%20application%20of%20different%20algorithms%20is%20helpful%20to%20solve%20this%20problem.%5B10%5D%20For%20example%2C%20using%20the%20Kamada%E2%80%93Kawai%20algorithm%5B11%5D%20to%20quickly%20generate%20a%20reasonable%20initial%20layout%20and%20then%20the%20Fruchterman%E2%80%93Reingold%20algorithm%5B12%5D%20to%20improve%20the%20placement%20of%20neighbouring%20nodes.
+        inital_pos = nx.kamada_kawai_layout(G)
+        final_pos = nx.fruchterman_reingold_layout(G, pos = inital_pos)
+        return final_pos
     
-class toplogies:
-    # the input topologies
-    att = 'ATT'
-    abilene = 'Abilene'
-    ibm = 'IBM'
-    surfnet = 'SURFnet'
-    g_50_p1 = 'G(50, 0.1)'
-    g_50_p05 = 'G(50, 0.05)'
-    pa_50_2 = 'PA(50, 2)'
-    pa_50_2 = 'PA(50, 3)'
+    'fruchterman reingold'
+    'circular'
+    'spectral': nx.random_layout,
+    'random': nx.spectral_layout,
+    'spring': nx.spring_layout, # internally in networkx is equal to 'fruchterman reingold'
+    'kamada kawai': nx.kamada_kawai_layout,
+    'planar': nx.planar_layout,
+    'spiral': nx.spiral_layout,
+    'kk --> fr'
 
-class storage_servers_selection:
-    manual = 'manual'
-    random = 'random'
-    degree = 'degree'
-
-class user_pairs_selection:
-    manual = 'manual'
-    random = 'random'
-
-class graph_layout:
-    fruchterman_reingold = 'fruchterman reingold'
-    circular = 'circular'
-    spectral = 'spectral'
-    random = 'random'
-    spring = 'spring'
-    kamada_kawaii = 'kamada kawai'
-    planar = 'planar'
-    spiral = 'spiral'
-    kk_into_fr = 'kk --> fr' # kamada kawai as inital layout fed into fruchterman reingold
+    _function_for = {
+        'fruchterman reingold': nx.fruchterman_reingold_layout,
+        'circular': nx.circular_layout,
+        'spectral': nx.random_layout,
+        'random': nx.spectral_layout,
+        'spring': nx.spring_layout, # internally in networkx is equal to 'fruchterman reingold'
+        'kamada kawai': nx.kamada_kawai_layout,
+        'planar': nx.planar_layout,
+        'spiral': nx.spiral_layout,
+        'kk --> fr': _kk_into_fr,
+        }
+        
+        function = _function_for[self.config.graph.layout.name]
