1d7bbe2bd8ec90fb28bb7b5e177c0973e8193606
Huzaifah Nadeem <huzaifahnadeem@gmail.com>
2023-10-18 17:40:53 -0400

    reset

--- config.py
+++
@@ -1,6 +0,0 @@
-class plot:
-    # config for the graph plot when visualizing
-    # 'width' x 'height' in inches s.t. each 1 inch is equal to 'dpi' pixels
-    width, height, dpi = 15, 9.75, 80
-
-    layout_choice = 
