a238533a5ebfd6b26a0dce9c7e709ed4e650c9fc
Huzaifah Nadeem <huzaifahnadeem@gmail.com>
2024-05-05 17:17:38 -0400

    minor refactoring

--- qpass-qcast/args.py
+++ qpass-qcast/args.py
@@ -1,7 +1,7 @@
 import argparse
 import globals
 
-def _args():
+def get_args():
     parser = argparse.ArgumentParser(description="Quantum Entanglement Routing Algorithms Implementation with Netsquid.")
 
     config = globals.Defaults # TODO: add option to use a config file (would contain a class with properties exactly like globals.Default class)
@@ -40,3 +40,43 @@ def _args():
     # TODO: arg for q probability.
     
     globals.args = parser.parse_args()
+
+def args_range_check(): # TODO: https://gist.github.com/dmitriykovalev/2ab1aa33a8099ef2d514925d84aa89e7 provides a better way
+    raise_exception = False
+    if globals.args.num_ts < 1:
+        raise_exception = True
+        param = '--num_ts'
+        sign = '>='
+        limit = '1'
+    elif globals.args.yen_n < 1:
+        raise_exception = True
+        param = '--yen_n'
+        sign = '>='
+        limit = '1'
+    elif globals.args.p3_hop < 1:
+        raise_exception = True
+        param = '--p3_hop'
+        sign = '>='
+        limit = '1'
+    elif globals.args.min_sd < 0:
+        raise_exception = True
+        param = '--min_sd'
+        sign = '>='
+        limit = '0'
+    elif globals.args.p2_nc < 1:
+        raise_exception = True
+        param = '--p2_nc'
+        sign = '>='
+        limit = '1'
+    elif not (globals.args.max_sd >= globals.args.min_sd):
+        raise_exception = True
+        param = '--max_sd'
+        sign = '>='
+        limit = "parameter '--min_sd'"
+    elif not globals.args.link_establish_timeout >= 0:
+        raise_exception = True
+        param = '--link_establish_timeout'
+        sign = '>='
+        limit = '0'
+    if raise_exception:
+        raise ValueError(f"The input parameter '{param}' must be {sign} {limit}.")
--- qpass-qcast/setup.py
+++ qpass-qcast/setup.py
@@ -12,45 +12,5 @@ def apply_args() -> None:
     numpy.random.seed(globals.args.seed) # used internally by networkx (maybe netsquid too but not sure)
 
 def get_args() -> None:
-    args._args()
-    args_range_check()
-    
-def args_range_check():
-    raise_exception = False
-    if globals.args.num_ts < 1:
-        raise_exception = True
-        param = '--num_ts'
-        sign = '>='
-        limit = '1'
-    elif globals.args.yen_n < 1:
-        raise_exception = True
-        param = '--yen_n'
-        sign = '>='
-        limit = '1'
-    elif globals.args.p3_hop < 1:
-        raise_exception = True
-        param = '--p3_hop'
-        sign = '>='
-        limit = '1'
-    elif globals.args.min_sd < 0:
-        raise_exception = True
-        param = '--min_sd'
-        sign = '>='
-        limit = '0'
-    elif globals.args.p2_nc < 1:
-        raise_exception = True
-        param = '--p2_nc'
-        sign = '>='
-        limit = '1'
-    elif not (globals.args.max_sd >= globals.args.min_sd):
-        raise_exception = True
-        param = '--max_sd'
-        sign = '>='
-        limit = "parameter '--min_sd'"
-    elif not globals.args.link_establish_timeout >= 0:
-        raise_exception = True
-        param = '--link_establish_timeout'
-        sign = '>='
-        limit = '0'
-    if raise_exception:
-        raise ValueError(f"The input parameter '{param}' must be {sign} {limit}.")
+    args.get_args()
+    args.args_range_check()
