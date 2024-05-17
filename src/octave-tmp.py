6bea862ae5814ed5a566e4efd5bd2b1d437d1dc4
Huzaifah Nadeem <huzaifahnadeem@gmail.com>
2024-05-17 13:42:16 -0400

    added a sample file on how to use quantinf lib using oct2py

--- src/octave-tmp.py
+++ src/octave-tmp.py
@@ -1,5 +1,11 @@
-from oct2py import octave # allows calling octave/matlab functions in python (need to install octave first): https://pypi.org/project/oct2py/
+# need to install octave and oct2py first.
+# oct2py: https://pypi.org/project/oct2py/
+# octave: https://wiki.octave.org/Octave_for_GNU/Linux
+
+from oct2py import octave # allows calling octave/matlab functions in python (octave needs to be installed in the system to use)
 import os 
+
+# Note: there is an issue with oct2py/io.py file line 376. it does not import spmatrix from scipy but adding "from scipy.sparse import spmatrix" before this line or at the top of file fixes the issue
 
 lib_path = f'{os.path.dirname(os.path.realpath(__file__))}/lib/quantinf/'
 octave.addpath(lib_path)
