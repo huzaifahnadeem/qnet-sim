84fcc01004d0f9eb0d5b269680f1a429d7964089
huzaifahnadeem <huzaifahnadeem@gmail.com>
2024-06-04 14:28:07 -0400

    run.py improvements (minor)

--- src/run.py
+++ src/run.py
@@ -30,19 +30,21 @@ def main():
     cmds_name = []
 
     # SLMP experiments repeat:
-    seeds = range(0, 10)
-    qs = ['0', '0.1']
-    ps = ['0.4', '0.55']
+    seeds = range(0, 20)
+    # qs = ['0', '0.1']
+    # ps = ['0.4', '0.55']
+    pqs = [('0.55', '0'), ('0.4', '0.1'), ('0.4', '0')]
     x_dists = range(1, 11)
 
+    exp_counter = 0
     for x in x_dists:
-        for p in ps:
-            for q in qs:
-                for s in seeds:
-                    cmds_name.append(f'seed={str(s)}, p={p}, q={q}')
-                    cmds.append(
-                        CMD_PREFIX + f"--seed={str(s)} --alg=SLMPg --network=grid_2d --grid_dim=11 --prob_swap_loss={q} --qc_loss_model=fixed --qc_p_loss_init={p} --x_dist_gte={str(x)} --x_dist_lte={str(x)} --y_dist_gte={str(x)} --y_dist_lte={str(x)} --max_sd=10 --min_sd=10"
-                    )
+        for p, q in pqs:
+            for s in seeds:
+                exp_counter += 1
+                cmds_name.append(f'exp_num {exp_counter} out of {len(list(seeds))*len(list(x_dists))*len(pqs)}')
+                cmds.append(
+                    CMD_PREFIX + f"--one_sided_epr --seed={str(s)} --alg=SLMPg --network=grid_2d --grid_dim=11 --prob_swap_loss={q} --qc_loss_model=fixed --qc_p_loss_init={p} --x_dist_gte={str(x)} --x_dist_lte={str(x)} --y_dist_gte={str(x)} --y_dist_lte={str(x)} --max_sd=10 --min_sd=10"
+                )
 
     args = []
     for idx, c in enumerate(cmds):
