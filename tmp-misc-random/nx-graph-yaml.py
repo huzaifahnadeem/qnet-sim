a4f9779753fb39db9e89c5de5e7e8efe3aebb416
huzaifahnadeem <huzaifahnadeem@gmail.com>
2024-06-17 15:30:07 -0400

    setting up for some experiments

--- tmp-misc-random/nx-graph-yaml.py
+++ tmp-misc-random/nx-graph-yaml.py
@@ -1,13 +1,38 @@
 import networkx as nx
 import yaml
-# import random
 
-# seed = 0
-# random.seed(seed)
+# choice = '0.55+half'
+# choice = '0.55+twice'
+# choice = '0.4+half'
+choice = '0.4+twice'
+
+
+len_55 = 17.34
+len_40 = 11.09
 
 dim = 19
-# l = [0.1, 0.25, 0.5, 0.75, 0.9]
-l = [0.5, 1.0]
+
+if choice == '0.55+half':
+    yaml_filename = "/home/hun13/qnet-sim/tmp-misc-random/grid_2d_19x19_varlen_p0.55_with_half.yaml"
+    length_factor = len_55
+    l = [0.5*length_factor, 1.0*length_factor]
+
+if choice == '0.55+twice':
+    yaml_filename = "/home/hun13/qnet-sim/tmp-misc-random/grid_2d_19x19_varlen_p0.55_with_twice.yaml"
+    length_factor = len_55
+    l = [2*length_factor, 1.0*length_factor]
+
+if choice == '0.4+half':
+    yaml_filename = "/home/hun13/qnet-sim/tmp-misc-random/grid_2d_19x19_varlen_p0.4_with_half.yaml"
+    length_factor = len_40
+    l = [0.5*length_factor, 1.0*length_factor]
+
+if choice == '0.4+twice':
+    yaml_filename = "/home/hun13/qnet-sim/tmp-misc-random/grid_2d_19x19_varlen_p0.4_with_twice.yaml"
+    length_factor = len_40
+    l = [2*length_factor, 1.0*length_factor]
+
+
 w = 1
 
 G = nx.grid_2d_graph(dim, dim)
@@ -26,7 +51,6 @@ for n in G.nodes:
 G = nx.relabel_nodes(G, map)
 
 graph_dict = nx.to_dict_of_dicts(G)
-yaml_filename = "/home/hun13/qnet-sim/src/networks-data/grid_2d_19x19_varlen_0.5_1.0.yaml"
 
 with open(yaml_filename, 'w') as outfile:
     yaml.dump(graph_dict, outfile)
---
+++ tmp-misc-random/verifying-xsep-64-72-degrees.py
@@ -0,0 +1,52 @@
+import yaml
+
+file_64 = '/home/hun13/qnet-sim/tmp-misc-random/64-degree-slmp-rep.yaml'
+file_72 = '/home/hun13/qnet-sim/tmp-misc-random/72-degree-slmp-rep.yaml'
+
+
+def _grid_dist(u, v, dir_ind):
+    u = u[1:-1].split(',')
+    v = v[1:-1].split(',')
+
+    return abs(int(u[dir_ind]) - int(v[dir_ind]))
+
+def _grid_x_dist(u, v):
+    return _grid_dist(u, v, 0) # node name's tuple = (x, y). x = idx 0, y = idx 1
+
+def _grid_y_dist(u, v):
+    return _grid_dist(u, v, 1) # node name's tuple = (x, y). x = idx 0, y = idx 1
+
+def grid_x_dist(pair):
+    return _grid_x_dist(pair[0], pair[1])
+
+def grid_y_dist(pair):
+    return _grid_y_dist(pair[0], pair[1])
+
+
+def tm_from_file(file_name):
+    # sample file: src/sample_tm_file.yaml
+
+    num_of_ts = 11
+    tm = [[] for _ in range(num_of_ts)]
+    with open(file_name) as tm_file:
+        tm_data = yaml.safe_load(tm_file)
+        for ts in range(1, num_of_ts + 1):
+            for sd_pair in tm_data[ts]:
+                sd = (sd_pair[0], sd_pair[1])
+                tm[ts-1].append(sd)
+        
+    return tm
+
+def print_seps(degree, file):
+    print(f"for {degree} degrees")
+    tm = tm_from_file(file_name=file)
+    for i, traffic in enumerate(tm):
+        ts = i + 1
+        print(f"ts = {ts}")
+        for pair in traffic:
+            print(f"{pair} => x-sep = {grid_x_dist(pair)} ; y-sep = {grid_y_dist(pair)}")
+        print()
+
+print_seps('64', file_64)
+print(); print(); print()
+print_seps('72', file_72)
